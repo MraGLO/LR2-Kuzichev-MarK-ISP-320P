d9a32394672641a1802c1418a64da09dd4075335
MraGLO <94346067+MraGLO@users.noreply.github.com>
2022-10-02 21:18:23 +0300

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -6,5 +6,5 @@ Person per2 = new Person("qwe", "rty", new DateTime(2004, 11, 30));
 Console.WriteLine(per1.GetHashCode());
 Console.WriteLine(per2.GetHashCode());
 
-Student stud1 = new Student(Education.Specialist, 105, new Test("math", true), new Exam("PE",4, new DateTime(2015,11,23)) );
+//Student stud1 = new Student(Education.Specialist, 105, new Test("math", true), new Exam("PE",4, new DateTime(2015,11,23)) );
 
